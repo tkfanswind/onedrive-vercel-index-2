658c376e20e4f2238c4e8966cfc61c3f6def5f52
tkfanswind <55653816+tkfanswind@users.noreply.github.com>
2024-09-16 00:29:59 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -38,7 +38,7 @@ module.exports = {
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
   // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
   footer:
-    '<p align="center"><a href="https://tkfans.us" target="_blank" rel="noopener noreferrer"><img src="https://img.shields.io/badge/tkfans%20One%20Drive-000000?logo=Apple&logoColor=FFFFFF" alt="tkfans"></a></p>',
+    '<p align="center"><a href="https://www.tkfans.us" target="_blank" rel="noopener noreferrer"><img src="https://img.shields.io/badge/tkfans%20One%20Drive-000000?logo=Apple&logoColor=FFFFFF" alt="tkfans"></a></p>',
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
