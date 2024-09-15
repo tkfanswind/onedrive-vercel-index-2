43cdc6f8913e54b061269b4f41870b22588b6d30
tkfanswind <55653816+tkfanswind@users.noreply.github.com>
2024-09-16 00:28:09 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -20,7 +20,7 @@ module.exports = {
   title: "Tetsuya Komuro OneDrive",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
-  baseDirectory: '/Public',
+  baseDirectory: '/Tetsuya Komuro',
 
   // [OPTIONAL] This represents the maximum number of items that one directory lists, pagination supported.
   // Do note that this is limited up to 200 items by the upstream OneDrive API.
@@ -38,11 +38,11 @@ module.exports = {
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
   // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
   footer:
-    'Powered by <a href="https://github.com/spencerwooo/onedrive-vercel-index" target="_blank" rel="noopener noreferrer">onedrive-vercel-index</a>. Made with ❤ by SpencerWoo.',
+    '<p align="center"><a href="https://tkfans.us" target="_blank" rel="noopener noreferrer"><img src="https://img.shields.io/badge/tkfans%20One%20Drive-000000?logo=Apple&logoColor=FFFFFF" alt="tkfans"></a></p>',
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/🌞 Private folder/u-need-a-password', '/🥟 Some test files/Protected route'],
+  protectedRoutes: ['/TK Music', '/TK Video', '/ISO'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: 'mailto:tkfanswind@gmail.com',
@@ -51,10 +51,22 @@ module.exports = {
   // In the latest update, all brand icons inside font awesome is supported and the icon to render is based on the name
   // you provide. See the documentation for details.
   links: [
-    {
+ {
       name: 'GitHub',
       link: 'https://github.com/spencerwooo/onedrive-vercel-index',
     },
+    {
+      name: 'YouTube',
+      link: 'https://www.youtube.com/c/tkfans1',
+    },
+    {
+      name: 'Telegram',
+      link: 'https://t.me/tkfans',
+    },
+    {
+      name: 'QQ',
+      link: 'http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=jygQ60zAWgqe5ZQtODPkkO2-QLdDrfdd&authKey=4WMdr7%2BX6efBAQ6Twc6cBrQs6980dsb1eSXEK3QpAnSQWaL9GGHJuOvDgjXnNu6b&noverify=0&group_code=4097583',
+    }
   ],
 
   // This is a day.js-style datetime format string to format datetimes in the app. Ref to
