3d7e8e1067db909f4c6124150e20a9877b565895
tkfanswind <55653816+tkfanswind@users.noreply.github.com>
2025-04-11 09:45:50 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -42,7 +42,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/NEW', '/ETC', '/Apps'],
+  protectedRoutes: ['/NEW', '/ETC', '/Music'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: 'mailto:tkfanswind@gmail.com',
